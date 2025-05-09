0549e6a0cec523b43dd5ff87729b55a92bf9a859
GS1020 <ooyopw123@Naver.com>
2025-05-09 17:51:59 +0900

    1

--- js/main.js
+++ js/main.js
@@ -1,33 +1,34 @@
 async function bookData1() {
-    const params = new URLSearchParams({
-      target: 'title',
-      query:"아빠 당신의 이야기를 들려주세요"
-      
+  const params = new URLSearchParams({
+    target: 'title',
+    query: "아빠 당신의 이야기를 들려주세요",
+
+  });
+
+  try {
+    const response = await fetch(`https://dapi.kakao.com/v3/search/book?${params}`, {
+      method: 'GET',
+      headers: {
+        Authorization: "KakaoAK bd7cb41ce43d371ae2e745f1c7ba9962"
+      }
     });
-  
-    try {
-      const response = await fetch(`https://dapi.kakao.com/v3/search/book?${params}`, {
-        method: 'GET',
-        headers: {
-          Authorization: "KakaoAK bd7cb41ce43d371ae2e745f1c7ba9962"
-        }
-      });
-  
-      if (!response.ok) {
-        throw new Error(`HTTP 오류 오류코드: ${response.status}`);
-      }
-  
-      const data = await response.json();
-  
-      if (data.documents.length === 0) {
-        $('.searchbook-container').append('<p>책 정보를 찾을 수 없습니다.</p>');
-        return;
-      }
-  
-      const book = data.documents[0];
-      const summary = book.contents.substring(0, 60);
-  
-      const html = `
+
+    if (!response.ok) {
+      throw new Error(`HTTP 오류 오류코드: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (data.documents.length === 0) {
+      $('.searchbook-container').append('<p>책 정보를 찾을 수 없습니다.</p>');
+      return;
+    }
+
+    const book = data.documents[0];
+    const summary = book.contents.substring(0, 60);
+
+
+    const html = `
         <div class="searchbook">
           <img src="${book.thumbnail}" alt="책 썸네일">
           <div class="text-area">
@@ -37,103 +38,146 @@ async function bookData1() {
           </div>
         </div>
       `;
-  
-      $('.searchbook-container').append(html);
-      
-    } catch (error) {
-      console.error('오류발생:', error);
-      $('.searchbook-container').append('<p>데이터 로드 중 오류 발생</p>');
-    }
-  }
-  
-  bookData1();
 
-  $(document).ready(function () {
-    let hideTimer;
-  
-    $('.nav3, .sub').on('mouseenter', function () {
-      clearTimeout(hideTimer); 
-      $('.sub').stop(true, true).css('display', 'flex').fadeIn(200); 
-    });
-  
-    $('.nav3, .sub').on('mouseleave', function () {
-      hideTimer = setTimeout(function () {
-        $('.sub').stop(true, true).fadeOut(200);
-      }, 300); 
-    });
+    $('.searchbook-container').append(html);
+
+  } catch (error) {
+    console.error('오류발생:', error);
+    $('.searchbook-container').append('<p>데이터 로드 중 오류 발생</p>');
+  }
+}
+
+bookData1();
+
+$(document).ready(function () {
+  let hideTimer;
+
+  $('.nav3, .sub').on('mouseenter', function () {
+    clearTimeout(hideTimer);
+    $('.sub').stop(true, true).css('display', 'flex').fadeIn(200);
   });
 
-  // $(document).ready(function(){
-  //   $('.kkbook li:first').mouseenter(function(){
-  //     $('.kkbook2').stop().fadeIn();
-  //     $('.kkbook3').stop().hide();
-  //     $('.kkbook4').stop().hide()
-  //   })
-  //   $('.kkbook li:nth-child(2)').mouseenter(function(){
-  //     $('.kkbook3').stop().css('display','flex').fadeIn();
-  //     $('.kkbook2').stop().hide();
-  //     $('.kkbook4').stop().hide()
-  //   });
-  //   $('.kkbook li:nth-child(3)').mouseenter(function(){
-  //     $('.kkbook4').stop().css('display','flex').fadeIn();
-  //     $('.kkbook2').stop().hide();
-  //     $('.kkbook3').stop().hide()
-  //   });
-  // });
-  $(document).ready(function () {
-    const boxes = ['.kkbook2', '.kkbook3', '.kkbook4'];
-  
-    $('.kkbook li').mouseenter(function () {
-      const index = $(this).index();
-      
-      // 모두 숨기고
-      boxes.forEach(box => $(box).stop().hide());
-  
-      // 해당 박스만 보여주기
-      if (boxes[index]) {
-        $(boxes[index]).stop().css('display', 'flex').fadeIn();
+  $('.nav3, .sub').on('mouseleave', function () {
+    hideTimer = setTimeout(function () {
+      $('.sub').stop(true, true).fadeOut(200);
+    }, 300);
+  });
+});
+
+
+$(document).ready(function () {
+  const boxes = ['.kkbook2', '.kkbook3', '.kkbook4'];
+
+  $('.kkbook li').mouseenter(function () {
+    const index = $(this).index();
+
+
+    boxes.forEach(box => $(box).stop().hide());
+
+
+    if (boxes[index]) {
+      $(boxes[index]).stop().css('display', 'flex').fadeIn();
+    }
+  });
+});
+
+async function bookData() {
+  const params = new URLSearchParams({
+    target: 'title',
+    query: '테일러',
+    size: 4
+
+  });
+
+  try {
+    const response = await fetch(`https://dapi.kakao.com/v3/search/book?${params}`, {
+      method: 'GET',
+      headers: {
+        Authorization: "KakaoAK bd7cb41ce43d371ae2e745f1c7ba9962"
       }
     });
-  });
-  
-  async function bookData() {
+
+
+    if (!response.ok) {
+      throw new Error(`HTTP 오류 오류코드: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (data.documents.length === 0) {
+      $('.searchbook-container').append('<p>책 정보를 찾을 수 없습니다.</p>');
+      return;
+    }
+
+
+
+    for (let i = 0; i < data.documents.length; i++) {
+      const book = data.documents[i];
+
+      $('.bigbook').append("<img src=" + book.thumbnail + ">")
+    }
+  } catch (error) {
+    console.error('오류발생:', error);
+    $('.searchbook-container').append('<p>데이터 로드 중 오류 발생</p>');
+  }
+}
+bookData();
+
+
+// 스와이프
+let appendNumber = 600;
+let prependNumber = 1;
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+
+// 섹션 베스트셀러
+
+async function bookData3() {
+  const boxes = $('.shortsbox');
+  const booksearch = ['니체', '단 한번의 삶'];
+
+  for (let i = 0; i < booksearch.length; i++) {
+    const query = booksearch[i];
+
     const params = new URLSearchParams({
       target: 'title',
-      query: '테일러',
-      size:4
-      
+      query: query
     });
-  
-    try {
-      const response = await fetch(`https://dapi.kakao.com/v3/search/book?${params}`, {
-        method: 'GET',
-        headers: {
-          Authorization: "KakaoAK bd7cb41ce43d371ae2e745f1c7ba9962"
-        }
-      });
-  
 
-      if (!response.ok) {
-        throw new Error(`HTTP 오류 오류코드: ${response.status}`);
+    const response = await fetch(`https://dapi.kakao.com/v3/search/book?${params}`, {
+      method: 'GET',
+      headers: {
+        Authorization: "KakaoAK bd7cb41ce43d371ae2e745f1c7ba9962"
       }
-  
-      const data = await response.json();
-  
-      if (data.documents.length === 0) {
-        $('.searchbook-container').append('<p>책 정보를 찾을 수 없습니다.</p>');
-        return;
-      }
-  
-     
+    });
+
+    const data = await response.json();
+    const book = data.documents[0];
+
+    if (book && boxes[i]) {
+      const box = boxes.eq(i);
+      box.html(""); // 혹시 중복 append 방지용 초기화
+      box.append(`<img src="${book.thumbnail}" alt="${book.title}">`);
+      box.append(`<p>BEST SELLER</p>`)
+      box.append(`<h6>${book.title}</h6>`);
       
-      for(let i=0;i<data.documents.length;i++){
-        const book = data.documents[i];
-       
-      $('.bigbook').append("<img src="+book.thumbnail+">")
-    }
-    } catch (error) {
-      console.error('오류발생:', error);
-      $('.searchbook-container').append('<p>데이터 로드 중 오류 발생</p>');
     }
   }
-  bookData();
+}
+
+bookData3();
+
+
