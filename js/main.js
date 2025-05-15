dc8b3111ddee3a55e122856de858d0d80e4c5256
GS1020 <ooyopw123@Naver.com>
2025-05-15 19:00:34 +0900

    1

--- js/main.js
+++ js/main.js
@@ -173,30 +173,30 @@ async function bookData3() {
       box.append(`<img src="${book.thumbnail}" alt="${book.title}">`);
       box.append(`<p>BEST SELLER</p>`)
       box.append(`<h6>${book.title}</h6>`);
-      
-      
+
+
     }
   }
 }
 bookData3();
 
 
-  // 날짜 자동으로 갱신
-  function upDate(){
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = today.getMonth()+1;
-    const date = today.getDate();
+// 날짜 자동으로 갱신
+function upDate() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const date = today.getDate();
 
-    document.getElementsByClassName('todaydate')[0].innerHTML=`${year}년 ${month}월 ${date}일`;
-  }
-  upDate();
+  document.getElementsByClassName('todaydate')[0].innerHTML = `${year}년 ${month}월 ${date}일`;
+}
+upDate();
 
 
 // 오늘의 책
 async function bookData4() {
   const boxes = $('.todaybox');
-  const booksearch = ['바움가트너', '과자 사면 과학 드립니다','닥터프렌즈의 구사일생','인구 충격 부동산','귀하신 몸'];
+  const booksearch = ['바움가트너', '과자 사면 과학 드립니다', '닥터프렌즈의 구사일생', '인구 충격 부동산', '귀하신 몸'];
 
   for (let i = 0; i < booksearch.length; i++) {
     const query = booksearch[i];
@@ -215,58 +215,83 @@ async function bookData4() {
 
     const data = await response.json();
     const book = data.documents[0];
-    
+
     if (book && boxes[i]) {
       const box = boxes.eq(i);
-      box.html(""); 
+
+      box.html("");
       box.append(`<img src="${book.thumbnail}" alt="${book.title}">`);
       box.append(`<h5>${book.title}</h5>`);
       box.append(`<h6>${book.authors}</h6>`);
 
-      
-      }
+      box.css('position', 'relative');
+      const ten = book.contents.substring(0, 190);
+
+      box.append(`<div class="overlay"
+        style="background-color:#333;width:100%;height:100%;opacity:0.9;color:#fff; position:absolute; padding:20px;top:0;left:0;overflow:auto; display:none"></div>`);
+      box.find('.overlay').append(`<p>${ten}</p>`)
+      box.find('.overlay').append(`<div class="overlaybox" style="display: flex; justify-content: space-around; gap: 10px; margin-top: 10px;"></div>`);
+
+
+      box.find('.overlaybox').append(`
+  <div class="view" style="width:40px; height:40px; color:#fff; border:2px solid #fff; line-height:35px; text-align:center;">
+    <p style="margin:0;">View</p>
+  </div>
+`);
+
+      box.find('.overlaybox').append(`
+  <div class="view" style="width:40px; height:40px; color:#fff; border:2px solid #fff; line-height:35px; text-align:center;">
+    <p style="margin:0;">Cart</p>
+  </div>
+`);
+      box.mouseenter(function () {
+        $(this).children('.overlay').stop().fadeIn();
+      }).mouseleave(function () {
+        $(this).children('.overlay').stop().fadeOut();
+      });
+    }
 
   }
 }
 bookData4();
 
-$(document).ready(function(){
-  function slide1(){
-    $('.gong2').stop().animate({marginTop:-40},800,function(){
+$(document).ready(function () {
+  function slide1() {
+    $('.gong2').stop().animate({ marginTop: -40 }, 800, function () {
       $('.gong2 li:first').appendTo('.gong2');
-      $('.gong2').css({marginTop:0});
+      $('.gong2').css({ marginTop: 0 });
     });
   }
-  setInterval(slide1,3000);
+  setInterval(slide1, 3000);
 });
 
 
 var swiper2 = new Swiper(".mySwiper2", {
-      slidesPerView: 1,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  effect: "coverflow",
+  coverflowEffect: {
+    slideShadows: false,
+  },
+  pagination: {
+    el: ".mySwiper2 .swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
       spaceBetween: 10,
-       effect: "coverflow",
-       coverflowEffect: {
-  slideShadows: false,
-},
-      pagination: {
-        el: ".mySwiper2 .swiper-pagination",
-        clickable: true,
-      },
-      breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-        },
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 10,
-        },
-      },
-    });
+    },
+  },
+});
 
 
 
@@ -294,12 +319,12 @@ async function bookData5() {
 
     const data = await response.json();
     const book = data.documents[0];
-    const ten = book.contents.substring(0,150);
-    
+    const ten = book.contents.substring(0, 150);
+
     if (book && boxes[i]) {
       const box = boxes.eq(i);
-     
-      box.html(""); 
+
+      box.html("");
       box.append(`<img src="${book.thumbnail}" alt="${book.title}">`);
       box.append(`<div class="textbox">
           <h5>${book.title}</h5>
@@ -308,8 +333,8 @@ async function bookData5() {
           </div>
         `);
 
-      
-      }
+
+    }
 
   }
 }
@@ -318,15 +343,15 @@ bookData5();
 // 화제의 책들 스와이퍼
 
 var swiper3 = new Swiper(".mySwiper3", {
-      slidesPerView: 5,
-      slidesPerGroup:5,
-      spaceBetween: 30,
-      freeMode: true,
-      pagination: {
-        el: ".mySwiper3-pagination",
-        clickable: true,
-      },
-    });
+  slidesPerView: 5,
+  slidesPerGroup: 5,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".mySwiper3-pagination",
+    clickable: true,
+  },
+});
 
 
 
@@ -335,8 +360,8 @@ var swiper3 = new Swiper(".mySwiper3", {
 async function bookData6() {
   const params = new URLSearchParams({
     target: 'title',
-    query:'셀러',
-    size:30
+    query: '셀러',
+    size: 30
   });
 
   try {
@@ -355,12 +380,38 @@ async function bookData6() {
     const boxx = $('.mySwiper3 .swiper-slide');
 
     for (let i = 0; i < 30; i++) {
-      const books = data.documents[i]; 
+      const box = $(boxx[i]);
+      const books = data.documents[i];
       $(boxx[i]).append(`<img src="${books.thumbnail}" alt="${books.title}">`);
       $(boxx[i]).append(`<div class=hotbox>
         <h5>${books.title}</h5>
         <h6>${books.authors}</h6>
         `)
+
+      box.css('position', 'relative');
+      const ten = books.contents.substring(0, 160);
+      box.append(`<div class="overlay"
+        style="background-color:#333;width:100%;height:273px;opacity:0.9;color:#fff; position:absolute; padding:20px;top:0;left:0;overflow:hidden; display:none"></div>`);
+      box.find('.overlay').append(`<p>${ten}</p>`)
+      box.find('.overlay').append(`<div class="overlaybox" style="display: flex; justify-content: space-around; gap: 10px; margin-top: 10px;padding:10px;"></div>`);
+
+
+      box.find('.overlaybox').append(`
+  <div class="view" style="width:40px; height:40px; color:#fff; border:2px solid #fff; line-height:35px; text-align:center;">
+    <p style="margin:0;">View</p>
+  </div>
+`);
+
+      box.find('.overlaybox').append(`
+  <div class="view" style="width:40px; height:40px; color:#fff; border:2px solid #fff; line-height:35px; text-align:center;">
+    <p style="margin:0;">Cart</p>
+  </div>
+`);
+      box.mouseenter(function () {
+        $(this).children('.overlay').stop().fadeIn();
+      }).mouseleave(function () {
+        $(this).children('.overlay').stop().fadeOut();
+      });
     }
   } catch (error) {
     console.error('오류 발생:', error);
@@ -373,7 +424,7 @@ bookData6();
 async function bookData7() {
   const boxes = $('.recent-bookbox');
   const booksearch = [
-    '내 꿈에 가끔만 놀러와','김켈리의 신비마트3','완벽주의자의 조용한 우울','모든 것이 양자 이론','고음질 명반 가이드북'
+    '내 꿈에 가끔만 놀러와', '김켈리의 신비마트3', '완벽주의자의 조용한 우울', '모든 것이 양자 이론', '고음질 명반 가이드북'
   ];
 
   for (let i = 0; i < booksearch.length; i++) {
@@ -393,12 +444,13 @@ async function bookData7() {
 
     const data = await response.json();
     const book = data.documents[0];
-    
-    
+
+
     if (book && boxes[i]) {
       const box = boxes.eq(i);
-     
-      box.html(""); 
+
+      box.html("");
+      box.css('position', 'relative');
       box.append(`<img src="${book.thumbnail}" alt="${book.title}">`);
       box.append(`<div class="textbox">
           <h5>${book.title}</h5>
@@ -406,9 +458,31 @@ async function bookData7() {
           
           </div>
         `);
+      const ten = book.contents.substring(0, 190);
+      box.append(`<div class="overlay"
+        style="background-color:#333;width:100%;height:100%;opacity:0.9;color:#fff; position:absolute; padding:20px;top:0;left:0; display:none"></div>`);
+      box.find('.overlay').append(`<p>${ten}</p>`)
+      box.find('.overlay').append(`<div class="overlaybox" style="display: flex; justify-content: space-around; gap: 10px; margin-top: 10px;"></div>`);
 
-      
-      }
+
+      box.find('.overlaybox').append(`
+  <div class="view" style="width:40px; height:40px; color:#fff; border:2px solid #fff; line-height:35px; text-align:center;">
+    <p style="margin:0;">View</p>
+  </div>
+`);
+
+      box.find('.overlaybox').append(`
+  <div class="view" style="width:40px; height:40px; color:#fff; border:2px solid #fff; line-height:35px; text-align:center;">
+    <p style="margin:0;">Cart</p>
+  </div>
+`);
+      box.mouseenter(function () {
+        $(this).children('.overlay').stop().fadeIn();
+      }).mouseleave(function () {
+        $(this).children('.overlay').stop().fadeOut();
+      });
+
+    }
 
   }
 }
@@ -416,22 +490,22 @@ bookData7();
 
 
 // 급상승 도서 스와이프
-    var swiper4 = new Swiper(".mySwiper4", {
-      slidesPerView: 5,
-      spaceBetween: 30,
-      slidesPerGroup:5,
-      freeMode: true,
-      pagination: {
-        el: ".mySwiper4-pagination",
-        clickable: true,
-      },
-    });
+var swiper4 = new Swiper(".mySwiper4", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  slidesPerGroup: 5,
+  freeMode: true,
+  pagination: {
+    el: ".mySwiper4-pagination",
+    clickable: true,
+  },
+});
 
 
 async function bookData8() {
   const boxes = $('.mySwiper4 .swiper-slide');
   const booksearch = [
-   '청춘의 독서','빛과 실','어른 김장하 각본','흔한남매 19','시간 유전자','너였구나','똥볶이 할멈','돈과 시간을 장악','채식주의자','작별하지 않는다','긴긴밤','별에게','별별 직업 상담소','마음의 기술','초등일타과학'
+    '청춘의 독서', '빛과 실', '어른 김장하 각본', '흔한남매 19', '시간 유전자', '너였구나', '똥볶이 할멈', '돈과 시간을 장악', '채식주의자', '작별하지 않는다', '긴긴밤', '별에게', '별별 직업 상담소', '마음의 기술', '초등일타과학'
   ];
 
   for (let i = 0; i < booksearch.length; i++) {
@@ -451,12 +525,12 @@ async function bookData8() {
 
     const data = await response.json();
     const book = data.documents[0];
-    
-    
+
+
     if (book && boxes[i]) {
       const box = boxes.eq(i);
-     
-      box.html(""); 
+
+      box.html("");
       box.append(`<img src="${book.thumbnail}" alt="${book.title}">`);
       box.append(`<div class="upbox">
           <h5>${book.title}</h5>
@@ -464,10 +538,153 @@ async function bookData8() {
           
           </div>
         `);
+      const ten = book.contents.substring(0, 160);
+      box.append(`<div class="overlay"
+        style="background-color:#333;width:188px;height:273px;opacity:0.9;color:#fff; position:absolute; padding:20px; display:none"></div>`);
+      box.find('.overlay').append(`<p>${ten}</p>`)
+      box.find('.overlay').append(`<div class="overlaybox" style="display: flex; justify-content: space-around; gap: 10px; margin-top: 10px; padding:10px;"></div>`);
 
-      
-      }
+
+      box.find('.overlaybox').append(`
+  <div class="view" style="width:40px; height:40px; color:#fff; border:2px solid #fff; line-height:35px; text-align:center;">
+    <p style="margin:0;">View</p>
+  </div>
+`);
+
+      box.find('.overlaybox').append(`
+  <div class="view" style="width:40px; height:40px; color:#fff; border:2px solid #fff; line-height:35px; text-align:center;">
+    <p style="margin:0;">Cart</p>
+  </div>
+`);
+      box.mouseenter(function () {
+        $(this).children('.overlay').stop().fadeIn();
+      }).mouseleave(function () {
+        $(this).children('.overlay').stop().fadeOut();
+      });
+
+    }
 
   }
 }
-bookData8();  
+bookData8();
+// hotbook 슬라이드
+var swiper5 = new Swiper(".mySwiper5", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  slidesPerGroup: 5,
+  pagination: {
+    el: ".mySwiper5-pagination",
+    clickable: true,
+  },
+  // freeMode: false,
+  // loop:false,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+
+//hot book 슬라이더
+
+async function bookData9() {
+  const boxes = $('.mySwiper5 .swiper-slide');
+  const booksearch = [
+    '엄마 당신의 이야기를 들려주세요', '아빠 당신의 이야기를 들려주세요', '더 퀸-', '나민애의 동시 읽기', '단 한 번의 삶', '니체 인생수업(리커버 에디션)', '슈뻘맨 무인 편의점', '슈팅스타 캐치 티니핑', '빛과 실- 2024', '청춘의 독서'
+  ];
+
+  for (let i = 0; i < booksearch.length; i++) {
+    const query = booksearch[i];
+
+    const params = new URLSearchParams({
+      target: 'title',
+      query: query
+    });
+
+    const response = await fetch(`https://dapi.kakao.com/v3/search/book?${params}`, {
+      method: 'GET',
+      headers: {
+        Authorization: "KakaoAK bd7cb41ce43d371ae2e745f1c7ba9962"
+      }
+    });
+
+    const data = await response.json();
+    const book = data.documents[0];
+
+
+    if (book && boxes[i]) {
+      const box = boxes.eq(i);
+
+      box.html("");
+      box.append(`<img src="${book.thumbnail}" alt="${book.title}">`);
+      box.append(`<div class="hotbookbox">
+          <h5>${book.title}</h5>
+          <h6>${book.authors}</h6>
+          </div>`
+
+      );
+      const ten = book.contents.substring(0, 160);
+      box.append(`<div class="overlay"
+        style="background-color:#333;width:188px;height:273px;opacity:0.9;color:#fff; position:absolute; padding:20px; display:none"></div>`);
+      box.find('.overlay').append(`<p>${ten}</p>`)
+
+      box.find('.overlay').append(`<div class="overlaybox" style="display: flex; justify-content: space-around; gap: 10px; margin-top: 10px; padding:10px"></div>`);
+
+
+      box.find('.overlaybox').append(`
+  <div class="view" style="width:40px; height:40px; color:#fff; border:2px solid #fff; line-height:35px; text-align:center;">
+    <p style="margin:0;">View</p>
+  </div>
+`);
+
+      box.find('.overlaybox').append(`
+  <div class="view" style="width:40px; height:40px; color:#fff; border:2px solid #fff; line-height:35px; text-align:center;">
+    <p style="margin:0;">Cart</p>
+  </div>
+`);
+
+
+
+      box.mouseenter(function () {
+        $(this).children('.overlay').stop().fadeIn();
+      }).mouseleave(function () {
+        $(this).children('.overlay').stop().fadeOut();
+      });
+
+    }
+
+  }
+}
+bookData9();  
+
+// 이런책은 어떠세요 슬라이더
+
+var swiper6 = new Swiper('.mySwiper6', {
+      slidesPerView: 3,
+      direction: getDirection(),
+      navigation: {
+        nextEl: '.mySwiper6-button-next',
+        prevEl: '.mySwiper6-button-prev',
+      },
+      on: {
+        resize: function () {
+          swiper.changeDirection(getDirection());
+        },
+      },
+    });
+
+    function getDirection() {
+      var windowWidth = window.innerWidth;
+      var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+      return direction;
+    }
